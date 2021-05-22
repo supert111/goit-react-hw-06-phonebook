c82f954bb9d50104cff8f14231ca93e4dce61703
supert111 <sportsmen@bigmir.net>
2021-05-22 20:25:11 +0300

    settings toolkit and redux-logger

--- src/redux/contactForm/contactForm-actions.js
+++ src/redux/contactForm/contactForm-actions.js
@@ -1,23 +1,44 @@
+import { createAction } from '@reduxjs/toolkit';
 import types from './contactForm-types';
 import shortid from 'shortid';
 
-const addContact = ({ name, number }) => ({
-    type: types.ADD,
-    payload: {
-        name,
-        number, 
-        id: shortid.generate(),
+const addContact = createAction(types.ADD, ({name, number}) => {
+    return {
+        payload: {
+            name,
+            number, 
+            id: shortid.generate(),
+        }
     }
 });
-
-const deleteContact = contactId => ({
-    type: types.DELETE,
-    payload: contactId,
-})
-
-const searchByFilter = (value) => ({
-    type: types.SEARCH_BY_FILTER,
-    payload: value,
-})
+const deleteContact = createAction(types.DELETE);
+const searchByFilter = createAction(types.SEARCH_BY_FILTER);
 
 export default { addContact, deleteContact, searchByFilter };
+
+///////////////////////////////////////////////////////////
+//redux
+
+// import types from './contactForm-types';
+// import shortid from 'shortid';
+
+// const addContact = ({ name, number }) => ({
+//     type: types.ADD,
+//     payload: {
+//         name,
+//         number, 
+//         id: shortid.generate(),
+//     }
+// });
+
+// const deleteContact = contactId => ({
+//     type: types.DELETE,
+//     payload: contactId,
+// })
+
+// const searchByFilter = (value) => ({
+//     type: types.SEARCH_BY_FILTER,
+//     payload: value,
+// })
+
+// export default { addContact, deleteContact, searchByFilter };
