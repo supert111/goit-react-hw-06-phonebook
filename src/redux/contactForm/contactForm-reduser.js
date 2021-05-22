54b86661b90779d8cd15148c4a1c395eb278b2ef
supert111 <sportsmen@bigmir.net>
2021-05-22 19:23:11 +0300

    start toolkit

--- src/redux/contactForm/contactForm-reduser.js
+++ src/redux/contactForm/contactForm-reduser.js
@@ -1,41 +1,42 @@
 import { combineReducers } from "redux";
 import types from './contactForm-types';
 
-const contacts = (state = [
+const contacts = (items = [
     {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
     {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
     {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
     {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-], {type, payload}) => {
+],
+ {type, payload}) => {
     switch(type) {
         case types.ADD:
-            const duplicateName = state.find(contact => contact.name.toLowerCase() === payload.name.toLowerCase());
+            const duplicateName = items.find(contact => contact.name.toLowerCase() === payload.name.toLowerCase());
             if(duplicateName) {
                 alert(`${payload.name} is already in contacts.`);
-                return state;
+                return items;
             }
 
             if(payload.name === "") {
                 alert(`Please fill out the form it is empty.`);
             }
 
-            return [payload, ...state,];
+            return [payload, ...items,];
 
 
         case types.DELETE:
-            return state.filter(({id}) => id !== payload)
+            return items.filter(({id}) => id !== payload)
 
         default:
-            return state; 
+            return items; 
     }
 };
 
-const filter = (state = '', {type, payload}) => {
+const filter = (items = '', {type, payload}) => {
     switch(type) {
         case types.SEARCH_BY_FILTER:
             return payload;
         default:
-            return state; 
+            return items; 
     } 
 };
 
